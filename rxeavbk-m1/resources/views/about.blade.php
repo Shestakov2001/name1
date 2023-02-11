3adf888ebdea2951c487fadbab97b6d2f5d06d78
Shestakov2001 <shestakov.jr@gmail.com>
2023-02-10 22:40:04 -0800

    Папапапа Серёжа молодец

--- rxeavbk-m1/resources/views/about.blade.php
+++ rxeavbk-m1/resources/views/about.blade.php
@@ -2,6 +2,55 @@
 
 @section('content')
 
-    
+            <div class="container">
+                <div class="logos">
+                    <img src="/public/img/logo.png" width="360px" height="360px">
+                    <p>У нас лучшие игры</p>
+                </div>
+            </div>
+
+            <div class="container">
+                <div id="carouselExampleCaptions" class="carousel slide">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                </div>
+                <div class="carousel-inner">
+                    @foreach($Products as $prod)
+                        @if($loop->index == 0)
+                            <div class="carousel-item active">
+                                <img src="/public/img/{{ $prod->photo}}" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>{{ $prod->name}}</h5>
+                                    <p>{{$prod->price}}</p>
+                                </div>
+                            </div>
+                            @else
+                            <div class="carousel-item active">
+                                <img src="/public/img/{{ $prod->photo}}" class="d-block w-100" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>{{ $prod->name}}</h5>
+                                    <p>{{$prod->price}}</p>
+                                </div>
+                            </div>
+
+                           
+                        @endif
+                    @endforeach
+                    
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Предыдущий</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Следующий</span>
+                </button>
+                </div>
+            </div>
 
 @endsection
--- rxeavbk-m1/resources/views/layouts/app.blade.php
+++ rxeavbk-m1/resources/views/layouts/app.blade.php
@@ -9,10 +9,13 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rell="stylesheet" type="text/css" href="resources/css/app.css">
    
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -79,5 +82,7 @@
             @yield('content')
         </main>
     </div>
+
+    
 </body>
 </html>
