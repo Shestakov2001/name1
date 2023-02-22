1dbdf6418680a89b670dabc40622027643432171
Shestakov2001 <shestakov.jr@gmail.com>
2023-02-21 23:12:26 -0800

    пупапупа Серёжа молодец

--- rxeavbk-m1/resources/views/soloproduct.blade.php
+++ rxeavbk-m1/resources/views/soloproduct.blade.php
@@ -1,45 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="container d-flex">
 
-            <div class="cards_solo">
+        <div class="cards_solo">
 
-                <img src="/public/img/{{$p->photo}}" alt="" class="catimg_solo img-fluid">
+            <img src="/public/img/{{ $p->photo }}" alt="" class="catimg_solo img-fluid">
 
-                    
-            </div>
-            
-            <div class="right">
 
-                <div class="cardsinfo">
-                            
-                    <div><p class="pros_name">{{$p->name}}</p></div>
+        </div>
 
-                    <div><p class="pros_price">{{$p->price}} руб.</p></div>
+        <div class="right">
 
-                    <button class="cupi">Купить</button>
+            <div class="cardsinfo">
 
+                <div>
+                    <p class="pros_name">{{ $p->name }}</p>
                 </div>
 
-                <br>
-                
-                <br>
-
-                <div class="character">
-                    
-
-                    <p class="pros">Харакетристики товара:</p>
-
-                    <p class="pros">Страна:{{$p->country}}</p>
-
-                    <p class="pros">Модель:{{$p->model}}</p>
-
-                    <p class="pros">Год выпуска:{{$p->year}}</p>
-
+                <div>
+                    <p class="pros_price">{{ $p->price }} руб.</p>
                 </div>
+
+                <button class="cupi">Купить</button>
+
             </div>
+
+            <br>
+
+            <br>
+
+            <div class="character">
+
+
+                <p class="pros">Харакетристики товара: </p>
+
+                <p class="pros">Страна: {{ $p->country }}</p>
+
+                <p class="pros">Модель: {{ $p->model }}</p>
+
+                <p class="pros">Год выпуска: {{ $p->year }}</p>
+
+            </div>
+        </div>
     </div>
-
 @endsection
